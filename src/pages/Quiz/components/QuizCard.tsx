482ac72f5fee750567b8baf60710cdbd319c8fc0
Jasur Rakhmanov <rakhmanovjasur0@gmail.com>
2025-11-17 16:24:45 +0500

    main fix:

--- src/pages/Quiz/components/QuizCard.tsx
+++ src/pages/Quiz/components/QuizCard.tsx
@@ -1,21 +1,21 @@
 import { FormControlLabel, Radio, Stack, Typography } from '@mui/material';
+import { useState } from 'react';
 import questions from '../questions.json';
 
 const QuizCard = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const currentQuestion = questions[currentQuestionIndex];
+
   return (
     <Stack direction='column' spacing={2}>
-      {questions.map((question, index) => (
-        <>
-          <Typography>
-            {index + 1}. {question.question}
-          </Typography>
-          <Stack direction='column' spacing={2}>
-            {question.options.map((answer, index) => (
-              <FormControlLabel key={index} control={<Radio />} label={answer} />
-            ))}
-          </Stack>
-        </>
-      ))}
+      <Typography>
+        {currentQuestionIndex + 1}. {currentQuestion.question}
+      </Typography>
+      <Stack direction='column' spacing={2}>
+        {currentQuestion.options.map((answer, index) => (
+          <FormControlLabel key={index} control={<Radio />} label={answer} />
+        ))}
+      </Stack>
     </Stack>
   );
 };
